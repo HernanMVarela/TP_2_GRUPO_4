337b37e2006c1d4b22ffd45fa3d7f7cf5eec91a6
HernanMVarela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-08-21 19:42:45 -0300

    Initial commit

---
+++ app/src/main/java/frgp/utn/edu/tp_2_grupo_4/ListadoContactosActivity.java
@@ -0,0 +1,14 @@
+package frgp.utn.edu.tp_2_grupo_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoContactosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_contactos);
+    }
+}
---
+++ app/src/main/java/frgp/utn/edu/tp_2_grupo_4/MainActivity.java
@@ -0,0 +1,49 @@
+package frgp.utn.edu.tp_2_grupo_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu_nav){
+        getMenuInflater().inflate(R.menu.menu_es_activity, menu_nav);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem opcion_menu){
+        int id = opcion_menu.getItemId();
+
+        if(id==R.id.agregarContacto){
+            navegar_agregar_contacto(null);
+            return true;
+        }
+        if(id==R.id.listarContacto){
+            navegar_listado_contactos(null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(opcion_menu);
+    }
+
+    public void navegar_agregar_contacto(android.view.View view) {
+        android.content.Intent intent = new android.content.Intent(this, AgregarContactoActivity.class);
+        startActivity(intent);
+    }
+
+    public void navegar_listado_contactos(android.view.View view) {
+        android.content.Intent intent = new android.content.Intent(this, ListadoContactosActivity.class);
+        startActivity(intent);
+    }
+
+}
